bea55168818ac4f1261e51a72727be20f952b684
Rory Stokes <contact@rorystok.es>
2016-04-12 22:13:14 +1000

    Added local storage of updoots

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var app = angular.module('upDooter', []);
-app.controller('game', function($scope) {
+var app = angular.module('upDooter', ['LocalStorageModule']);
+app.controller('game', function($scope, localStorageService) {
   $scope.updoots = 0;
-
+  localStorageService.bind($scope, 'updoots')
   $scope.updoot = function(){
     $scope.updoots ++;
     console.log($scope.updoots);
