c8de4d2a0c55a7ce4387a3caac87bfb8b49c8113
Rory Stokes <contact@rorystok.es>
2016-04-13 14:09:54 +1000

    Adding in post mechanics

--- scripts/game.js
+++ scripts/game.js
@@ -3,9 +3,31 @@
 var app = angular.module('upDooter', ['LocalStorageModule']);
 app.controller('game', function($scope, localStorageService) {
   $scope.updoots = 0;
-  localStorageService.bind($scope, 'updoots')
-  $scope.updoot = function(){
-    $scope.updoots ++;
-    console.log($scope.updoots);
+  localStorageService.bind($scope, 'updoots');
+
+  var maxPosts = 5;
+
+  $scope.posts = [];
+
+  $scope.doot = function(post){
+    if(post.updooted){
+      //undoot
+      $scope.updoots --;
+      post.updooted = false;
+    }
+    else {
+      //updoot
+      $scope.updoots ++;
+      post.updooted = true;
+    }
+  };
+  var posts = 1;
+  $scope.post = function() {
+    var post = {text: "NEW POST"+posts, updooted: false};
+    $scope.posts.unshift(post);
+    if($scope.posts.length>maxPosts){
+      $scope.posts.pop();
+    }
+    posts ++;
   };
 });
