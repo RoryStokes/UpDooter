b3c8b688d00a1e14e8c6b9321c50a47a3aafdef8
Rory Stokes <contact@rorystok.es>
2016-05-01 09:31:47 +1000

    Repushing ImageService

--- scripts/services/ImageService.js
+++ scripts/services/ImageService.js
@@ -8,20 +8,19 @@ angular.module('upDooter').service('ImageService', function($rootScope) {
     return {
         getImage: function(post, tags) {
             var query = _.sample(tags);
-            imgur.search(query)
-                .then(function(data) {
-                    if(data.length > 1){
-                        var image = _.sample(data);
-                        $rootScope.$apply(function(){
-                            post.image = image.link;
-                        });
-                    }else{
-                        count ++;
-                        $rootScope.$apply(function(){
-                            post.image = 'http://lorempixel.com/500/380/?'+count;
-                        });
-                    }
-                });
+            imgur.search(query).then(function(data) {
+                if(data.length > 1){
+                    var image = _.sample(data);
+                    $rootScope.$apply(function(){
+                        post.image = image.link;
+                    });
+                }else{
+                    count ++;
+                    $rootScope.$apply(function(){
+                        post.image = 'http://lorempixel.com/500/380/?'+count;
+                    });
+                }
+            });
         }
     };
 });
