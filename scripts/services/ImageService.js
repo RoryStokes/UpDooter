a603c9c8ecdd8235e56a654368293f54a37ed037
Rory Stokes <contact@rorystok.es>
2016-05-04 20:24:04 +1000

    Imgur image feeding

--- scripts/services/ImageService.js
+++ scripts/services/ImageService.js
@@ -1,24 +1,41 @@
 /**
  * Created by Rory on 30/04/2016.
  */
-angular.module('upDooter').service('ImageService', function($rootScope) {
-    var imgur = window.imgur;
-    var count = 0;
+angular.module('upDooter').service('ImageService', function(ImgurMap) {
+    //var imgur = window.imgur;
+    //var imageSearch = window.imageSearch;
+    //var count = 0;
+    var _ = window._;
+    var random = window.random;
+    var keyword_opts = {
+        language:"english",
+        remove_digits: true,
+        return_changed_case:true,
+        remove_duplicates: true
+    };
+
+    var countIntersection = function(a, b){
+        return _.intersection([a,b]).length;
+    };
+
+    var pickImage = function(tags){
+        var keywords = [];
+        tags.forEach(function(tag){
+            keywords = keywords.concat(window.keyword.extract(tag, keyword_opts));
+        });
+        var source = _.pick(ImgurMap, keywords);
+        if(_.isEmpty(source)){
+            source = ImgurMap;
+        }
+        return _.chain(source)
+            .flatMap()
+            .sample()
+            .value()
+    };
 
     return {
         getImage: function(post, tags) {
-            count ++;
-            post.image = 'http://lorempixel.com/500/380/?'+count;
-            /*var query = _.sample(tags);
-            imgur.search(query).then(function(data) {
-                if(data.length > 1){
-                    var image = _.sample(data);
-                    $rootScope.$apply(function(){
-                        post.image = image.link;
-                    });
-                }else{
-                }
-            });*/
+            post.image = 'http://i.imgur.com/' + pickImage(tags) + 'l.png';
         }
     };
 });
