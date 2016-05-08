442a340351f4616e6dcb610e7538a5f12fda894c
Rory Stokes <contact@rorystok.es>
2016-05-08 22:00:49 +1000

    good

---
+++ scripts/controllers/Config.js
@@ -0,0 +1,11 @@
+angular.module('upDooter').config(function($routeProvider) {
+    $routeProvider.
+    when('/post/:postDetails', {
+        templateUrl: 'postDetails.html',
+        controller: 'PostDetails'
+    }).
+    otherwise({
+        templateUrl: 'feed.html',
+        controller: 'Feed'
+    });
+});
